6e4e265986ffc019125111c5e8591901eb0a3a01
Débora Chagas <deb.chagas@gmail.com>
2023-07-01 19:27:51 -0300

    Ajusta ci

--- mix.exs
+++ mix.exs
@@ -16,7 +16,8 @@ defmodule Arvore.MixProject do
         coveralls: :test,
         "coveralls.detail": :test,
         "coveralls.post": :test,
-        "coveralls.html": :test
+        "coveralls.html": :test,
+        "coveralls.lcov": :test
       ]
     ]
   end
